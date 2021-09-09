3e726e36aee56ee5d7fd8226463846753d84c0e3
Hemant <hemant_patil1@persistent.com>
2021-09-09 15:20:49 +0530

    file added

---
+++ File.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main() {
+    string s;
+    string s1;
+    ofstream out("text.txt");
+
+    cout<<"Enter data which you want to add into file :";
+    getline(cin,s);
+
+    out<<s;
+    out.close();
+
+    ifstream in("text.txt");
+    in>>s1;
+
+    cout<<"Data :"<<s1;
+
+    return 0;
+}
